65f8bdec785b9d3feab44bd7ef8be91c534e0d64
Mongkol YANGKORN <zenith.1328@outlook.com>
2021-07-16 23:01:03 +0700

    js strucet add text show and delete

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const TodoTitle = document.querySelector('.title');
-const TodoButton = document.querySelector('.addtodo');
-const todoList = document.querySelector('.tweet');
+const TodoTitle = document.querySelector('.todo-title');
+const TodoButton = document.querySelector('.add-todo');
+const todoList = document.querySelector('.todo-list');
 
 function removeTodo(event){
     event.target.parentNode.remove();
 }
 
+function likeTodo(event){
+    let Count = 0;
+    Count++;
+    console.log(Count);
+    event.target.parentNode.add(Count);
+}
+
+
+function addtodo(){
+
+    // div
+    const todo = document.createElement('div');
+    todo.classList.add('todo');
+
+    // h3
+    const h3 = document.createElement('h3');
+    h3.innerHTML = TodoTitle.value;
+
+    // tweet , delete button
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.classList.add('remove-Todo');
+    button.innerHTML = 'Delete';
+    button.addEventListener('click', removeTodo);
+
+    // 
+
+    // h3 , button -> div
+    todo.append(h3, button);
+
+    // div + todo list
+    todoList.prepend(todo);
+}
+TodoButton.addEventListener('click', addtodo);
